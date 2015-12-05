b916fa0f00f5423b055a97b8659e1a515a86d007
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-12-04 18:15:24 -0800

    fixes #1 stringify fails when element is a number ; fixes #2 stringify fails when element is a number

--- test/stringify.js
+++ test/stringify.js
@@ -3,12 +3,34 @@
 var expect = require('chai').expect,
     lib = require('../lib');
 
+var dat = {
+    t0: {
+        src: ['text', {a: 55}, ['tspan', 'so me']],
+        dst: '<text a="55">\n  <tspan>so me</tspan>\n</text>\n'
+    },
+    t1: {
+        src: ['A',
+            'aaa',
+            ['B',
+                ['C', true],
+                777,
+                ['D',
+                    ['E']
+                ]
+            ]
+        ],
+        dst: '<A>\n  aaa\n  <B>\n    <C>true</C>\n    777\n    <D>\n      <E/>\n    </D>\n  </B>\n</A>\n'
+    }
+};
+
 describe('stringify', function () {
-    it('basic', function (done) {
-        var val = ['text', {a: '55'}, ['tspan', 'so me']];
-        var str = '<text a="55">\n  <tspan>\n    so me\n  </tspan>\n</text>\n';
-        expect(lib.stringify(val)).to.be.equal(str);
-        done();
+    Object.keys(dat).forEach(function (name) {
+        it(name, function (done) {
+            var src = dat[name].src;
+            var dst = dat[name].dst;
+            expect(lib.stringify(src)).to.be.equal(dst);
+            done();
+        });
     });
 });
 
